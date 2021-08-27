d16c00eb26fd58ae364b74816158dba50e25896b
paradon61143227 <75900713+paradon61143227@users.noreply.github.com>
2021-08-27 17:04:51 +0700

    Coins1,2,5,10

--- pages/sale/index.vue
+++ pages/sale/index.vue
@@ -14,15 +14,28 @@
         <b-col col md="12">
           <h2>Insert coins</h2>
           <div class="insert" id="app">
-            <b-button v-on:click="data.coins1 += 1">Add 1</b-button>
-            <b-button v-on:click="data.coins2 += 2">Add 2</b-button>
-            <b-button v-on:click="data.coins5 += 5">Add 5</b-button>
-            <b-button v-on:click="data.coins10 += 10">Add 10</b-button>
-            <h2>
-              coins :
-              {{ data.coins1 + data.coins2 + data.coins5 + data.coins10 }}
-            </h2>
-            <b-button v-on:click="total(10)">Add</b-button>
+            <b-button @click="append(1)" class="btn">Coins 1</b-button>
+            <b-button @click="append(2)" class="btn">Coins 2</b-button>
+            <b-button @click="append(5)" class="btn">Coins 5</b-button>
+            <b-button @click="append(10)" class="btn">Coins 10</b-button>
+            <b-button @click="clear" variant="warning">Clear</b-button>
+            <b-button href="/" variant="danger">Exit</b-button>
+            <h3>Total Coins : {{ total }}</h3>
+            <b-row id="b-row">
+              <div v-for="pro in pro.data" :key="pro.data">
+                <div class="pro-info" v-if="pro.in_stock == true">
+                  <div class="pro-info" v-if="total >= pro.price">
+                    <b-col col lg="12">
+                      <div class="product" align="center">
+                        <h4>ชื่อสินค้า : {{ pro.name }}</h4>
+                        <h4>ราคา : {{ pro.price }} บาท</h4>
+                        <img :src="pro.image" />
+                      </div>
+                    </b-col>
+                  </div>
+                </div>
+              </div>
+            </b-row>
           </div>
         </b-col>
       </b-row>
@@ -36,27 +49,24 @@
 
 <script>
 export default {
+  data() {
+    return {
+      total: 0,
+    };
+  },
+  methods: {
+    clear() {
+      this.total = 0;
+    },
+    append(number) {
+      this.total = this.total + number;
+    },
+  },
   async asyncData({ $axios }) {
     const pro = await $axios.$get(
       "https://www.mocky.io/v2/5c77c5b330000051009d64c9"
     );
     return { pro };
-  },
-  data() {
-    return {
-      data: {
-        coins1: 0,
-        coins2: 0 * 2,
-        coins5: 0 * 5,
-        coins10: 0 * 10,
-      },
-    };
-  },
-  methode: {
-    total: function(coins) {
-        this.coins = coins;
-        console.log(coins);
-    },
   },
 };
 </script>
@@ -66,8 +76,20 @@ export default {
   margin: 1%;
 }
 
-.insert {
-  background-color: aquamarine;
-  height: 10px;
+.b-row {
+  margin-top: 5%;
+}
+
+.product {
+  background-color: #ffedda;
+  border-radius: 10px;
+  padding: 10px;
+  margin-top: 5%;
+  height: 400px;
+}
+
+img {
+  border-radius: 10px;
+  width: 100;
 }
 </style>
